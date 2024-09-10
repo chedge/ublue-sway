d62fd68499e2a9c3988ce1e60d51a680b222f1ab
C H <hedges.colin@gmail.com>
2024-09-10 00:46:52 -0700

    install command is probably more trouble than it's worth

--- installers/install-keyd.sh
+++ installers/install-keyd.sh
@@ -6,10 +6,13 @@ git clone https://github.com/rvaiya/keyd /tmp/keyd
 make -C /tmp/keyd
 
 # manual install, Makefile install doesn't like installing inside a container
-sed 's|@PREFIX@|/usr/local|g' /tmp/keyd/keyd.service.in > /tmp/keyd.service
+sed 's|@PREFIX@|/usr|g' /tmp/keyd/keyd.service.in > /tmp/keyd.service
 install -Dm644 /tmp/keyd.service /usr/lib/systemd/system/keyd.service
 
 groupadd keyd
+
+mkdir -p /usr/share/doc/keyd/examples
+mkdir -p /usr/share/keyd/layouts
 
 install -m755 /tmp/keyd/bin/* /usr/bin/
 install -m644 /tmp/keyd/docs/*.md /usr/share/doc/keyd/
