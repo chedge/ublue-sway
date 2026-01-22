6989fdbe3a00474743e357132afcb2478d196b75
C H <hedges.colin@gmail.com>
2026-01-22 15:32:17 -0800

    Updated bcvk installer to properly name binary and move to /usr/local/bin

--- build_files/installers/install-bcvk.sh
+++ build_files/installers/install-bcvk.sh
@@ -2,4 +2,5 @@
 
 set -ouex pipefail
 
-curl -sL https://github.com/bootc-dev/bcvk/releases/latest/download/bcvk-x86_64-unknown-linux-gnu.tar.gz | tar -xvzf - -C /usr/bin/
+curl -sL https://github.com/bootc-dev/bcvk/releases/latest/download/bcvk-x86_64-unknown-linux-gnu.tar.gz | tar -xvzf - -C /tmp/
+mv /tmp/bcvk-x86_64-unknown-linux-gnu /usr/local/bin/bcvk
